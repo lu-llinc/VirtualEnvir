b99fa2dcc8f7481105d9c3da42903cff340a70f0
Jasper Ginn <j.h.ginn@cdh.leidenuniv.nl>
2015-12-02 19:48:08 +0000

    provision.sh edited online with Bitbucket

--- provision.sh
+++ provision.sh
@@ -4,7 +4,7 @@
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' |
-tee /etc/apt/sources.list.d/mongodb.list
+sudo tee /etc/apt/sources.list.d/mongodb.list
 
 # Update
 sudo apt-get update
